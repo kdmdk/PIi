1464e1b55ed76360a1c3dc94d37e4e19ed5a8f07
kdmdk <hzmd8274@gmial.com>
2018-02-23 21:35:48 +0900

    PinBallプロジェクト　１回目のコミット
    スコアの表示まで完了しているファイルです。

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	
+	private int score = 0; //スコア計算用変数
+	//public string scoretxt;
+
+	//scoreを表示するテキスト
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score  = 10;
+		this.scoreText = GameObject.Find("ScoreText");
+	}
+
+	//接触時の処理
+	void OnCollisionEnter( Collision collision ){
+		string yourTag  = collision.gameObject.tag;
+
+		if (yourTag == "LargeCloudTag") {
+			score += 4;
+		} else if (yourTag == "LargeStarTag") {
+			score += 3;
+		} else if (yourTag == "SmallStarTag") {
+			score += 1;
+		} else if (yourTag == "SmallCloudTag") {
+			score += 2;
+		}
+	}
+	void Update () {
+		this.scoreText.GetComponent<Text>().text = "SCORE：" + score.ToString ();
+	}
+}
